a4a0f1a12c923c1b76fe0ce540017c99f1456e1c
julito16 <jcfigueredo.21@est.ucab.edu.ve>
2026-05-21 21:04:45 -0400

    Import Map in IntercambioController

--- cuidared-backend/src/main/java/com/cuidared/controllers/IntercambioController.java
+++ cuidared-backend/src/main/java/com/cuidared/controllers/IntercambioController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+import java.util.Map;
 /**
  * Controlador REST para la gestión de intercambios (solicitudes de cuidado).
  */
